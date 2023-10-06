bug backprop

src/main.py
@@ -7,6 +7,7 @@ import numpy as np
 import json
 
 from neuralnet.Encoder import MyEncoder
+from neuralnet.Trainer import Trainer
 
 image = cv2.imread("251.jpeg")
 
@@ -86,9 +87,14 @@ if result <= 0.5:
 else:
     category = "Panda"
 
-print(category)
+# print(category)
 
-print(newModel.layers[5].weights.shape)
+# print(newModel.layers[5].weights.shape)
+
+# print(newModel.layers[4].weights)
+trainer = Trainer(newModel, 1, 0.1, np.array([image]), np.array([[0]]))
+trainer.fit()
+# print(newModel.layers[4].weights)
 
 
 # model = NN(image.shape)
@@ -112,4 +118,4 @@ print(newModel.layers[5].weights.shape)
 
 # flat_shape = model.layers[4].feature_map_shape
 # model.add(Dense(1, 64, "sigmoid"))
-# model.save_model("")
+# model.save_model("yeah.json", 4)

src/neuralnet/Dense.py
@@ -54,6 +54,9 @@ class Dense(Layer):
         return self.feature_map_shape
     
     def backpropagate(self, out: np.ndarray, learn_rate: float):
-        dout = np.dot(out, self.last_input)
-        self.biases -= learn_rate * dout
+        # print(out.shape)
+        # print(self.last_input.shape)
+        dout =  out * self.last_input
+        # print(dout.shape)
+        self.weights -= learn_rate * dout
         return dout

src/neuralnet/NN.py
@@ -26,14 +26,54 @@ class NN:
             # print(image.shape)
         return image
     
-    def back_propagate(self, input, label, learning_rate):
+
+    def calculate_derr_error(self, layer: Layer, preds: np.ndarray, labels: np.ndarray):
+        if(len(preds) != len(labels)):
+            raise Exception(f"The label: {len(labels)} and prediction: {len(preds)} does not have same length")
+        # print("shape")
+        # print(np.sum(labels - preds).shape)
+        # print(layer.detector_function.deriv(layer.last_input, preds).shape)
+        # print(layer.last_input.shape)
+        return preds - labels
+        
+            
+    
+    def backpropagate(self, input, label, learning_rate):
         layers_count = len(self.layers)
         if(layers_count <= 0):
             raise Exception("There is no layers to backpropagate")
+        result = self.forward_propagate(input)
         prev_layer = self.layers[layers_count-1]
-        last_deriv = prev_layer.detector_function.deriv(prev_layer.last_input, label)
+        last_deriv = None
+        # print("yeah")
+        # print(last_deriv.shape)
+        # print(last_deriv)
         for i in range(layers_count-1, 0, -1):
-            print("yeah")
+            layer = self.layers[i]
+            
+            #CASE OUTPUT LAYER (OBVIOUSLY DENSE LAYER)
+            if(i == layers_count-1):
+                last_deriv = self.calculate_derr_error(prev_layer, result, label)
+                layer.backpropagate(last_deriv, learning_rate)
+
+            # CASE HIDDEN LAYER
+            else:
+                #CASE TYPE DENSE
+                if(layer.type == "dense"):
+                    print(layer.detector_function.name)
+                    print(layer.detector_function.deriv(layer.last_input).shape) #drelu2/dx3
+                    print(prev_layer.weights.shape) #dnet/drelu2
+                    print(last_deriv.shape) #deD/dnet
+                    print(layer.last_input.shape) #dx3/dw2
+
+                    print(layer.weights.shape)
+                    last_deriv = layer.detector_function.deriv(layer.last_input * layer.weights) * prev_layer.weights * last_deriv
+                    # print(last_deriv.shape)
+                    layer.backpropagate(last_deriv, learning_rate)
+                    prev_layer = layer
+                else:
+                    print("yeah")
+                                
     
     
     def toJSON(self):
@@ -44,8 +84,8 @@ class NN:
         ind = ""
         for i in range(indent):
             ind+= " "
-        print(ind)
-        print(self.input_shape.__class__)
+        # print(ind)
+        # print(self.input_shape.__class__)
         with open(filename, 'w') as f:
             f.write("{\n")
             f.write(f'''{ind}"input_shape": {list(self.input_shape)},\n''')